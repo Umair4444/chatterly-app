9772e26179b054c81d61b1009fe22da73870d7e1
Umair Ali Khan <100639296+Umair4444@users.noreply.github.com>
2025-09-16 15:19:17 +0500

    update

--- firebase.ts
+++ firebase.ts
@@ -9,20 +9,22 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCmHmi0jkRP7vUKELwLGfV1vCnn3aHbOiI",
-  authDomain: "chatterly-app-3e649.firebaseapp.com",
-  projectId: "chatterly-app-3e649",
-  storageBucket: "chatterly-app-3e649.firebasestorage.app",
-  messagingSenderId: "775396834101",
-  appId: "1:775396834101:web:8c16a9296adc9ad8a2aa7c",
-  measurementId: "G-4H37XMTHWL",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_APIKEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECTID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGE_SENDERID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APPID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENTID,
 };
+
+console.log(process.env.FIREBASE_APIKEY);
 
 // Initialize Firebase
 // const app = initializeApp(firebaseConfig);
 const app = getApps().length ? getApps()[0] : initializeApp(firebaseConfig);
 const auth = getAuth(app);
-const db = getFirestore(app)
+const db = getFirestore(app);
 const analytics = getAnalytics(app);
 
-export {auth,db,analytics}
+export { auth, db, analytics };
